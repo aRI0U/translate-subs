1df23a82165221a6da26b54405fd6e07ce6d21c8
Alain Riou <alain.riou14000@yahoo.com>
2022-08-21 13:25:27 +0200

    fixes

--- split/score.py
+++ split/score.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 import numpy as np
 
@@ -6,57 +6,94 @@ import spacy
 
 
 class ClauseSplitter:
-    def __init__(self, model: str = "en_core_web_trf"):
+    def __init__(
+            self,
+            model: str = "en_core_web_trf",
+            alpha: float = 0.1,
+            power_syntactic: int = 2,
+            power_positional: int = 2
+    ):
         self.syntactic_model = spacy.load(model)
 
+        self.alpha = alpha
+        self.power_syntactic = power_syntactic
+        self.power_positional = power_positional
+
         self.span = None
-        self.syntactic_loss = None
+        self._syntactic_loss = None
         self.positional_loss = None
 
-    def compute_span(self, sentence: str):
+    def compute_spans(self, sentence: str) -> List:
         doc = self.syntactic_model(sentence)
-        return next(doc.sents)
+        return [span for span in doc.sents]
 
-    def compute_loss(self, sentence: str, span, ratio: Optional[float] = None):
-        syntactic_loss = self.compute_syntactic_loss(sentence, span)
+    def compute_loss(self, sentence: str, spans, ratio: Optional[float] = None):
+        syntactic_loss = self.compute_syntactic_loss(sentence, spans)
+        print(syntactic_loss)
 
         if ratio is None:
             return syntactic_loss
 
-        positional_loss = self.compute_positional_loss(sentence, ratio)
-        positional_loss = self._expand_positional_loss()
-        loss = syntactic_loss + positional_loss  # TODO: clean this
+        positional_loss = self.compute_positional_loss(sentence, ratio, np.isinf(syntactic_loss))
+        print(positional_loss)
 
+        loss = syntactic_loss + self.alpha * positional_loss
+        print(loss)
         return loss
 
+    def compute_split_indices(self, sentence: str, ratio: Optional[float] = None):
+        spans = self.compute_spans(sentence)
+        loss = self.compute_loss(sentence, spans, ratio=ratio)
+        ranking = np.argsort(loss)
+
+        all_indices = []
+        for span in spans:
+            after_token_indices = np.zeros(len(span), dtype=int)
+            for i in range(len(span)):
+                token = span[i]
+                after_token_indices[i] = token.idx + len(token)
+
+            all_indices.append(after_token_indices)
+
+        all_indices = np.concatenate(all_indices)[:-1]
+        return all_indices[ranking]
+
     # SYNTACTIC LOSS
-    def compute_syntactic_loss(self, sentence, span):
-        self.syntactic_loss = np.zeros(len(span) - 1)
-        self._compute_syntactic_loss(span.root)
-        self.syntactic_loss = self._clean_syntactic_loss(sentence, span, self.syntactic_loss)
-        return self.syntactic_loss
+    def compute_syntactic_loss(self, sentence, spans):
+        num_tokens = sum(len(span) for span in spans)
+        self._syntactic_loss = np.zeros(num_tokens)
+        offset = 0
+
+        for span in spans:
+            self._compute_syntactic_loss(span.root)
+            self._clean_syntactic_loss(sentence, span, offset=offset)
+            offset += len(span)
+
+        # loss =
+        loss, self._syntactic_loss = self._syntactic_loss, None
+        return loss[:-1]
 
     def _compute_syntactic_loss(self, token):
         for left in token.lefts:
-            self.syntactic_loss[left.i: token.i] += 1
+            self._syntactic_loss[left.i: token.i] += 1
             self._compute_syntactic_loss(left)
         for right in token.rights:
-            self.syntactic_loss[token.i: right.i] += 1
+            self._syntactic_loss[token.i: right.i] += 1
             self._compute_syntactic_loss(right)
 
-    @staticmethod
-    def _clean_syntactic_loss(sentence, span, scores):
+    def _clean_syntactic_loss(self, sentence, span, offset=0):
         for i in range(len(span) - 1):
             token = span[i]
-            if sentence[token.idx + len(token)] != ' ':
-                scores[i] = np.inf
-        return scores
+            next_char_idx = token.idx + len(token)
+            if sentence[next_char_idx] != ' ' or sentence[next_char_idx+1] in '!?:;':
+                self._syntactic_loss[i + offset] = np.inf
 
     # POSITIONAL LOSS
-    def compute_positional_loss(self, sentence: str, ratio: float):
+    def compute_positional_loss(self, sentence: str, ratio: float, inf_mask: np.ndarray):
         spaces_indices = self._get_space_indices(sentence)
-        self.positional_loss = np.abs(spaces_indices - ratio * len(sentence))
-        return self.positional_loss
+        positional_loss = np.abs(spaces_indices - ratio * len(sentence))
+        positional_loss = self._expand_positional_loss(positional_loss, inf_mask)
+        return positional_loss ** self.power_positional
 
     @staticmethod
     def _get_space_indices(sentence: str) -> np.ndarray:
@@ -66,25 +103,32 @@ class ClauseSplitter:
                 spaces_indices.append(i)
         return np.array(spaces_indices)
 
-    def _expand_positional_loss(self):
-        positional_loss = np.zeros_like(self.syntactic_loss) + np.inf
+    @staticmethod
+    def _expand_positional_loss(positional_loss, inf_mask):
+        # TODO: compute this efficiently
+        expanded_loss = np.full(inf_mask.shape, np.inf)
         j = 0
-        for i, loss in enumerate(self.syntactic_loss):
-            if not np.isinf(loss):
-                positional_loss[i] = self.positional_loss[j]
+        for i, inf in enumerate(inf_mask):
+            if not inf:
+                expanded_loss[i] = positional_loss[j]
                 j += 1
-        self.positional_loss = positional_loss
-        return self.positional_loss
+        return expanded_loss
 
 
 if __name__ == "__main__":
-    splitter = ClauseSplitter("fr_dep_news_trf")
-    query = u"Tout d'abord, vers 8h30, M. Torakura s'est rendu au bureau " \
-            u"et Tadokoro, Ran et Conan sont allés chercher un film dans " \
-            u"la salle de collecte, c'est-à-dire juste derrière eux."
-    span = splitter.compute_span(query)
+    import deplacy
 
-    loss = splitter.compute_loss(query, span, ratio=0.3)
+    splitter = ClauseSplitter("fr_dep_news_trf", alpha=1e-2)
+    # query = u"Tout d'abord, vers 8h30, M. Torakura s'est rendu au bureau " \
+    #         u"et Bernard, Ran et Conan sont allés chercher un film dans " \
+    #         u"la salle de collecte, c'est-à-dire juste derrière eux."
+    # query = u"Qu'est-ce que vous faîtes ici ?! Partez immédiatement !"
+    query = u"Hmmm... Je vois."
+    doc = splitter.syntactic_model(query)
+    deplacy.render(doc)
 
-    for t, c in zip(span, loss):  # splitter.syntactic_loss):
-        print(t, '\n\t', c)
+    indices = splitter.compute_split_indices(query, ratio=0.5)
+    print(query)
+    for i, idx in enumerate(indices):
+        print(i+1)
+        print(query[:idx] + " / " + query[idx+1:])
---
+++ tune_hparams.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from pprint import pprint
+from tqdm import tqdm
+
+import numpy as np
+
+from split.score import ClauseSplitter
+
+
+def tune_model(config):
+    splitter = ClauseSplitter("fr_dep_news_trf", **config)
+    counter = Counter()
+    with open("data/newline_dataset.txt", 'r') as f:
+        for line in tqdm(f.readlines(), total=38352):
+            sep_idx = line.index(r'\N')
+            sentence = line.replace(r'\N', ' ').strip()
+            indices = splitter.compute_split_indices(sentence, ratio=0.5)
+            try:
+                rank = np.where(indices == sep_idx)[0][0]
+            except IndexError:
+                print(line)
+                print(indices, sep_idx)
+                if len(input()) > 0:
+                    raise IndexError
+            counter[rank] += 1
+    pprint(counter)
+
+
+if __name__ == "__main__":
+    tune_model({})

a948e72953b2fa2b11690f5b588c509c0e6ff416
Alain Riou <alain.riou14000@yahoo.com>
2022-08-29 00:29:08 +0200

    avoid invalid patterns"

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -7,13 +7,20 @@ import pysubs2
 
 def filter_subs(subs, out_file):
     tag_regex = re.compile(r"{\\.*?}")
+    spaces_regex = re.compile(" +")
     for line in subs:
         # 1. filter non-valid subs
         if line.style != "Default" or line.text.isupper():
             continue
 
         # 2. Remove tags
-        text = re.sub(tag_regex, "", line.text)
+        text = re.sub(tag_regex, "", line.text.strip())
+
+        # 3. Correct invalid patterns
+        text = re.sub(spaces_regex, " ", text)
+        text = text.replace(r" \N", r"\N")
+        if text.endswith(r'\N'):
+            text = text[:-2]
 
         # Write the result
         out_file.write(text + '\n')

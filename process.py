4475a23a2bb0eef28bba6f36aabb408d66c0564e
Alain Riou <alain.riou14000@yahoo.com>
2022-09-21 00:42:59 +0200

    enable splitter in processor

--- process.py
+++ process.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from tqdm import tqdm
 from typing import Optional, Sequence, Union
 
-import pysubs2
+from pysubs2 import SSAEvent, SSAFile
 
 from callbacks.base import Callback
 from translate import Translator
@@ -14,17 +14,19 @@ class SubtitlesProcessor:
             self,
             translator: Optional[Translator] = None,
             splitter: Optional[ClauseSplitter] = None,
-            callbacks: Optional[Sequence[Callback]] = None
+            callbacks: Optional[Sequence[Callback]] = None,
+            dialogue_limit: int = 2000
     ):
         self.translator = translator
         self.splitter = splitter
         self.callbacks = callbacks if callbacks is not None else []
+        self.dialogue_limit = dialogue_limit
 
         self.translate_text = self._translate_text if translator is not None else lambda x: x
 
     def process_subtitles(self, in_file: Union[Path, str], out_file: Union[str, Path]):
-        subs = pysubs2.load(in_file)
-
+        subs = SSAFile.load(in_file)
+        subs.sort()
         try:
             if self.splitter is None:
                 processed_subs = self._process_subs_wo_split(subs)
@@ -33,23 +35,73 @@ class SubtitlesProcessor:
         finally:
             processed_subs.save(out_file)
 
-    def _process_subs_wo_split(self, subs):
-        for line in tqdm(subs):
-            line.text = self.process_text(line.text, line.style)
+    def _process_subs_wo_split(self, subs: SSAFile) -> SSAFile:
+        new_subs = SSAFile()
+        for event in tqdm(subs):
+            processed_event = self.process_event(event)
+            new_subs.append(processed_event)
+        return new_subs
 
-    def _process_subs_w_split(self, subs):
-        pass
+    def _process_subs_w_split(self, subs: SSAFile) -> SSAFile:
+        new_subs = SSAFile()
+        clauses = []
 
-    def process_text(self, text: str, style):
+        for event in tqdm(subs):  # TODO: use next/iter to aboid testing len(clauses) all the time
+            if len(clauses) > 0 and self.sentence_split(clauses[-1], event):
+                new_subs.extend(self.process_events(clauses))
+                clauses = []
+            clauses.append(event)
+        new_subs.extend(self.process_events(clauses))
+
+        return new_subs
+
+    def process_event(self, event: SSAEvent) -> SSAEvent:
         for callback in self.callbacks:
-            text = callback.on_before_translate(text)
+            event = callback.on_before_translate(event)
 
-        text = self.translate_text(text)
+        event.text = self.translate_text(event.text)
 
         for callback in reversed(self.callbacks):
-            text = callback.on_after_translate(text)
+            event = callback.on_after_translate(event)
 
-        return text
+        return event
 
-    def _translate_text(self, text):
+    def process_events(self, events: Sequence[SSAEvent]) -> Sequence[SSAEvent]:
+        processed_event = self.process_event(self.merge_events(events))
+
+        start, duration = processed_event.start, processed_event.duration
+        text = processed_event.text
+
+        idx_list = [0]
+        for e in events[:-1]:
+            ratio = (e.end - start) / duration
+            indices = self.splitter.compute_split_indices(text, ratio=ratio)
+            if len(indices) == 0:
+                break
+            idx_list.append(indices[0])
+        idx_list.append(len(text))
+
+        for i in range(len(idx_list)-1):
+            idx1, idx2 = idx_list[i], idx_list[i+1]
+            events[i].text = text[idx1:idx2]
+
+        return events
+
+    def _translate_text(self, text: str) -> str:
         return self.translator.translate_text(text.replace(r"\N", " "))
+
+    def sentence_split(self, event1: SSAEvent, event2: SSAEvent) -> bool:
+        if event1.plaintext[-1] in ".?!\"":
+            return True
+        if event2.plaintext[0].isupper():
+            return event2.start - event1.end > self.dialogue_limit
+        return event2.start - event1.end > 2*self.dialogue_limit
+
+    @staticmethod
+    def merge_events(events: Sequence[SSAEvent]) -> SSAEvent:
+        return SSAEvent(
+            start=events[0].start,
+            end=events[-1].end,
+            text=' '.join(e.text for e in events),
+            style=events[0].style
+        )
